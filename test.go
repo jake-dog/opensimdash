3884aec579b04e47399cd966c6dc6b3e7095ca4c
Jake Comer <jake-dog@users.noreply.github.com>
2019-06-22 11:04:24 -0400

    fiddling

--- test.go
+++ test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/karalabe/hid"
+	"github.com/zetamatta/go-getch"
 )
 
 func getDevice() (*hid.Device, error) {
@@ -17,7 +18,32 @@ func getDevice() (*hid.Device, error) {
 	return nil, errors.New("Unable to find device")
 }
 
+func debugger() {
+	for _, dev := range hid.Enumerate(0x16c0, 0x0480) {
+		if dev.UsagePage == 0xFF31 && dev.Usage == 0x0074 {
+			c, err := dev.Open()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			data := make([]byte, 64)
+			for {
+				if i, err := c.Read(data); err != nil {
+					fmt.Println(err)
+					break
+				} else {
+					fmt.Println("Debugger read: ", i)
+					fmt.Println(string(data[:i]))
+				}
+			}
+		}
+	}
+	fmt.Println("Unable to find debugger")
+}
+
 func main() {
+	go debugger()
+
 	c, err := getDevice()
 	if err != nil {
 		fmt.Println(err)
@@ -33,12 +59,23 @@ func main() {
 		fmt.Println(data[:i])
 	}
 
-	sndto[0] = 103 // "g" in decimal
-	//sndto[0] = 99 // "c" in decimal
-	fmt.Println(sndto)
-	if i, err := c.Write(sndto); err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(i)
+	for v := 0; v < 10; v++ {
+		e := getch.Rune()
+		setting := byte(e & 0xFF)
+		if e >= 32 {
+			fmt.Println("received char: ", e, setting)
+			sndto[0] = setting
+			if i, err := c.Write(sndto); err != nil {
+				fmt.Println(err)
+				break
+			} else {
+				fmt.Println(i)
+			}
+		}
+
+		//sndto[0] = e
+		//sndto[0] = 103 // "g" in decimal
+		//sndto[0] = 99 // "c" in decimal
+		//fmt.Println(sndto)
 	}
 }
