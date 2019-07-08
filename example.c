3d6b351f8b60943ea9bedddcbda6abb2784271bf
Jake Comer <jake-dog@users.noreply.github.com>
2019-07-08 01:09:06 -0400

    removing commented code

--- example.c
+++ example.c
@@ -31,14 +31,11 @@
 
 #define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
 
-volatile uint8_t do_output=0;
 uint8_t buffer[64];
 
 int main(void)
 {
 	int8_t r;
-	//uint8_t i;
-	//uint16_t val, count=0;
 
 	// set for 16 MHz clock
 	CPU_PRESCALE(0);
@@ -52,12 +49,6 @@ int main(void)
 	// Wait an extra second for the PC's operating system to load drivers
 	// and do whatever it does to actually be ready for input
 	_delay_ms(1000);
-
-        // Configure timer 0 to generate a timer overflow interrupt every
-        // 256*1024 clock cycles, or approx 61 Hz when using 16 MHz clock
-        //TCCR0A = 0x00;
-        //TCCR0B = 0x05;
-        //TIMSK0 = (1<<TOIE0);
 
 	print("Begin rawhid example program\n");
 	while (1) {
@@ -73,48 +64,8 @@ int main(void)
 			phex(buffer[0]);
 			print("\n");
 		}
-		// if time to send output, transmit something interesting
-		/*if (do_output) {
-			do_output = 0;
-			// send a packet, first 2 bytes 0xABCD
-			buffer[0] = 0xAB;
-			buffer[1] = 0xCD;
- 			// put A/D measurements into next 24 bytes
-			for (i=0; i<12; i++) {
-				val = analogRead(i);
-				buffer[i * 2 + 2] = val >> 8;
-				buffer[i * 2 + 3] = val & 255;
-			}
-			// most of the packet filled with zero
-			for (i=26; i<62; i++) {
-				buffer[i] = 0;
-			}
-			// put a count in the last 2 bytes
-			buffer[62] = count >> 8;
-			buffer[63] = count & 255;
-			// send the packet
-			usb_rawhid_send(buffer, 50);
-			print("transmit packet ");
-			phex16(count);
-			print(", r=");
-			phex(r);
-			print("\n");
-			count++;
-		}*/
 	}
 }
-
-// This interrupt routine is run approx 61 times per second.
-/*ISR(TIMER0_OVF_vect)
-{
-	static uint8_t count=0;
-
-	// set the do_output variable every 2 seconds
-	if (++count > 122) {
-		count = 0;
-		do_output = 1;
-	}
-}*/
 
 
 
