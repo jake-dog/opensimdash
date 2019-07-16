f194e4d4eb7365691a34c3381454a1410bd53a3d
Jake Comer <jake-dog@users.noreply.github.com>
2019-07-16 01:00:29 -0400

    fixing telemetry test to avoid firewall warnings when running tests

--- telemetry_test.go
+++ telemetry_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestNewTelemetry(t *testing.T) {
-	c, err := NewTelemetry(":20777")
+	c, err := NewTelemetry("127.0.0.1:20777")
 	if err != nil {
 		t.Error(err)
 	}
