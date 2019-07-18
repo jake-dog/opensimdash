2e3b2701b4e7ec82bf829301a7875c6022ace28b
Jake Comer <jake-dog@users.noreply.github.com>
2019-07-18 00:15:56 -0400

    correcting documentation of TelemetryPack

--- hid/telemetrypack.go
+++ hid/telemetrypack.go
@@ -1,7 +1,7 @@
 package hid
 
-// TelemetryPack is the interface for a structure sent to the Write method of an
-// HID device which fullfills the HIDWriter interface.  It contains methods
+// TelemetryPack is the interface for a structure sent to the SendPack method of
+// an HID device which fullfills the HIDWriter interface.  It contains methods
 // which can be used to generate a binary payload to be sent to an HID device.
 type TelemetryPack interface {
 	GetGear() int
