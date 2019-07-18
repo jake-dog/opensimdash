2d913a4fb0371551cd616626f693fdb8d62d4e15
Jake Comer <jake-dog@users.noreply.github.com>
2019-07-17 22:28:47 -0400

    removing cruft from teensy code

--- hid/teensy.go
+++ hid/teensy.go
@@ -6,12 +6,11 @@ import (
 	"github.com/karalabe/hid"
 )
 
-var (
+func init() {
 	// Teensy is the regular teensy 2.0++.  Just sending rev light indicator using
 	// 64-byte payloads, but it only sets the first byte to reflect the 8 LEDs
-	Teensy = &teensy{
+	Register(&teensy{
 		snd:    make([]byte, 64),
-		leds:   make([]byte, 8),
 		levels: []int{80, 83, 85, 87, 89, 91, 93, 95},
 		SimDashDevice: &SimDashDevice{
 			DeviceInfo: &hid.DeviceInfo{
@@ -21,11 +20,11 @@ var (
 				Usage:     0x200,
 			},
 		},
-	}
+	})
 
 	// TeensyDebug is a separate Usage/UsagePage possibly used for sending debug
 	// messages.  It doesn't receive data, only sends it.
-	TeensyDebug = &teensy{
+	/*Register(&teensy{
 		SimDashDevice: &SimDashDevice{
 			DeviceInfo: &hid.DeviceInfo{
 				VendorID:  0x16c0,
@@ -34,47 +33,32 @@ var (
 				Usage:     0x0074,
 			},
 		},
-	}
-)
-
-func init() {
-	Register(Teensy)
-	//Register(TeensyDebug)
+	})*/
 }
 
 type teensy struct {
-	snd         []byte
-	leds        []byte
-	levels      []int
-	ledByte     byte
-	prevLedByte byte
+	snd     []byte
+	levels  []int
+	ledByte byte
 	*SimDashDevice
-}
-
-func setLeds(leds []byte, levels []int, signal int) {
-	for i, level := range levels {
-		if signal >= level {
-			leds[i] = 1
-		} else {
-			leds[i] = 0
-		}
-	}
 }
 
 func (t *teensy) Write(p TelemetryPack) {
 	// Compute which of the eight LEDs to turn on based on the revLightPercent
-	setLeds(t.leds, t.levels, p.GetRevLightPercent())
+	revLights := p.GetRevLightPercent()
 	t.ledByte = 0
-	for i, b := range t.leds {
-		t.ledByte |= b << uint(i)
+	for i, level := range t.levels {
+		if revLights >= level {
+			t.ledByte |= 1 << uint(i)
+		}
+	}
+
+	// Skip sending the HID payload if current and last byte are zero
+	if t.snd[0]|t.ledByte == 0 {
+		return
 	}
 	t.snd[0] = t.ledByte
 
-	// Skip sending the HID payload if current and last byte are zero
-	if t.prevLedByte|t.ledByte == 0 {
-		return
-	}
-	t.prevLedByte = t.ledByte
 	if _, err := t.Device.Write(t.snd); err != nil {
 		// TODO figure out the logging . . .
 		fmt.Println(err)
